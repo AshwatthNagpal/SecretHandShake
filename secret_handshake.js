9153f8edb3bcf6e4712648d880b2fde81c67c560
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-17 23:07:50 +0530

    write code to evaluate decimal 4  as close your eyes

--- secret_handshake.js
+++ secret_handshake.js
@@ -2,5 +2,8 @@ export const secretHandshake = (decimalNumber) => {
   if (decimalNumber === 1) {
     return ['wink'];
   }
-  return ['double blink'];
+  if (decimalNumber === 2) {
+    return ['double blink'];
+  }
+  return ['close your eyes'];
 };
